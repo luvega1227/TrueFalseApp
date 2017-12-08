4fb4383529dfd457eef7febcb5b5fcf4ed0dee2e
luvega1227 <luvega1227@gmail.com>
2017-12-07 18:23:53 -0800

    add OptionQuestionsVC

--- TrueFalseStarter/OptionQuestionsVC.swift
+++ TrueFalseStarter/OptionQuestionsVC.swift
@@ -10,21 +10,25 @@ import UIKit
 
 class OptionQuestionsVC: UIViewController {
     
-    // MARK: Buttons & Labels
-
+    // MARK: Outles
     @IBOutlet weak var emptySpace: UILabel!
     @IBOutlet weak var threeOptionQuestionsButton: UIButton!
     @IBOutlet weak var fourOptionQuestionsButton: UIButton!
     
     
-    
-
     // MARK: App Life Cycle
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        appStartUp()
+        emptySpace.text = ""
+        
+        // Buttons Corner Radius
+        threeOptionQuestionsButton.layer.cornerRadius = 6
+        fourOptionQuestionsButton.layer.cornerRadius = 6
+        
+        // Buttons Alpha color
+        threeOptionQuestionsButton.alpha = 1.0
+        fourOptionQuestionsButton.alpha = 1.0
         
 
         // Do any additional setup after loading the view.
@@ -35,29 +39,15 @@ class OptionQuestionsVC: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    
-    
-    // MARK: User's Trivia Choice Display
-    
-    func appStartUp() {
-        
-        emptySpace.text = ""
-        
-        threeOptionQuestionsButton.layer.cornerRadius = 6
-        fourOptionQuestionsButton.layer.cornerRadius = 6
-        
-    }
 
-    
-    
-    // MARK: Actions
-    
-    @IBAction func threeOptionTrivia(_ sender: Any) {
-        
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
     }
-    
-    @IBAction func fourOptionTrivia(_ sender: Any) {
-    }
-    
+    */
 
 }
